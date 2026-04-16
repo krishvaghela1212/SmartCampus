4df204434abbf18ddae0f9ed2301c6e4f432c296
Krish Vaghela <krishvaghela454@gmail.com>
2026-04-16 15:30:59 +0530

    Liveness

--- Server/src/server.js
+++ Server/src/server.js
@@ -88,8 +88,9 @@ const startServer = async () => {
     origin: [
       "http://localhost:3000",
       "http://localhost:5173",
-      
-      "https://smart-campus-git-krishv-krishs-projects-0c925a03.vercel.app/"
+      "https://smart-campus-git-krishv-krishs-projects-0c925a03.vercel.app/",
+      "https://smart-campus-git-main-krishs-projects-0c925a03.vercel.app",
+      "https://smart-campus-k4ofpyils-krishs-projects-0c925a03.vercel.app"
     ],
     credentials: true,
     methods: ['GET', 'POST', 'OPTIONS'],
